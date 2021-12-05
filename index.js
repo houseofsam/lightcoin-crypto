b26712b4256bb410e0cab26b082d32cca6191309
houseofsam <samxsaid@gmail.com>
2021-12-05 05:42:08 +0000

    Finally managed to resolve the issue where if a withdrawal transaction was permitted, it'd add onto the balance instead of subtract. The solution seems a little make-shift though (had to create a new property for the Transaction objects 'valueAmount' and assigned it the value from the getters in Deposit & Withdrawal. This solution also lets you deposit/withdraw negative amounts, but that should be an easy fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 class Account {
   constructor(username) {
     this.username = username;
-    // this.balance = 0;
     this.transactions = [];
   }
 
@@ -13,18 +12,11 @@ class Account {
     let balance = 0;
 
     for (let i = 0; i < this.transactions.length; i++) {
-      balance += this.transactions[i].amount;
+      balance += this.transactions[i].valueAmount;
     }
 
     return balance;
-
-    // couldn't get reduce to work :(
-    // return this.transactions[amount]reduce((accumulator, current) => {
-    //   return accumulator +  current;
-    // });
   }
-
-
 }
 
 class Transaction {
@@ -32,15 +24,6 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
-
-  // isAllowed(tf) {
-  //   if (tf = true) {
-  //     this.time = new Date();
-  //     this.account.addTransaction(this);
-  //   } else {
-  //     return 'Transaction not permitted! Check balance.'
-  //   }
-  // }
 
   isAllowed() {
     if (this.value < 0 && this.account.balance >= this.amount) {
@@ -54,25 +37,19 @@ class Transaction {
 
 
   commit() {
-    // this.isAllowed();
 
     if (this.isAllowed()) {
       this.time = new Date();
+      this.valueAmount = this.value;
       this.account.addTransaction(this);
     } else {
       return 'Transaction not permitted!'
     }
+
   }
 }
 
-
-
-
 class Withdrawal extends Transaction {
-
-  // constructor() {
-  //   this.isAllowed();
-  // }
 
   get value() {
     return -this.amount;
@@ -85,7 +62,6 @@ class Deposit extends Transaction {
   get value() {
     return this.amount;
   }
-
 
 }
 
@@ -107,13 +83,14 @@ const myAccount = new Account('snow-patrol');
 
 // console.log('Balance:', t2.balance);
 
-t3 = new Deposit(120.00, myAccount);
+t3 = new Deposit(-120.00, myAccount);
 t3.commit();
 t4 = new Deposit(80.00, myAccount);
 t4.commit();
-t4 = new Withdrawal(1900.00, myAccount);
+t4 = new Withdrawal(-10.00, myAccount);
 t4.commit();
 // console.log('Transaction 3:', t3);
 console.log('Account Summary:', myAccount);
 console.log('Transaction Summary:', t4);
+console.log('Transaction Summary:', t4.value);
 console.log('Balance ->', myAccount.balance);
