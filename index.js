bfaa8546c5e58ce9fd1f4d188c48598d77425aad
houseofsam <samxsaid@gmail.com>
2021-12-05 05:25:27 +0000

    Almost complete. Was able to keep track of transactions, but had difficulties implementing code to validate withdrawals (Managed to get the validator to either return true if the request > balance or false if not, but when the withdrawal transaction takes place if the condition is true, a deposit is made (balance increases)..)

--- index.js
+++ index.js
@@ -1,14 +1,91 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    // this.balance = 0;
+    this.transactions = [];
   }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+  get balance() {
+    let balance = 0;
+
+    for (let i = 0; i < this.transactions.length; i++) {
+      balance += this.transactions[i].amount;
+    }
+
+    return balance;
+
+    // couldn't get reduce to work :(
+    // return this.transactions[amount]reduce((accumulator, current) => {
+    //   return accumulator +  current;
+    // });
+  }
+
+
+}
+
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  // isAllowed(tf) {
+  //   if (tf = true) {
+  //     this.time = new Date();
+  //     this.account.addTransaction(this);
+  //   } else {
+  //     return 'Transaction not permitted! Check balance.'
+  //   }
+  // }
+
+  isAllowed() {
+    if (this.value < 0 && this.account.balance >= this.amount) {
+      return true;
+    } else if (this.value > 0){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
 
   commit() {
-    balance -= this.amount;
+    // this.isAllowed();
+
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    } else {
+      return 'Transaction not permitted!'
+    }
   }
+}
+
+
+
+
+class Withdrawal extends Transaction {
+
+  // constructor() {
+  //   this.isAllowed();
+  // }
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
 
 }
 
@@ -18,12 +95,25 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account('snow-patrol');
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// console.log('Transaction 1:', t1);
 
-console.log('Balance:', balance);
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// console.log('Balance:', t2.balance);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+t4 = new Deposit(80.00, myAccount);
+t4.commit();
+t4 = new Withdrawal(1900.00, myAccount);
+t4.commit();
+// console.log('Transaction 3:', t3);
+console.log('Account Summary:', myAccount);
+console.log('Transaction Summary:', t4);
+console.log('Balance ->', myAccount.balance);
